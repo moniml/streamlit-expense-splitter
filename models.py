9f52efceaffa1ca7d46d9d9e9a7a7fb4a9738d47
Monica <monicamuthaa@gmail.com>
2025-08-03 21:40:09 +0530

    Add requirements.txt and code files

--- models.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, name):
-        self.name = name
-
-class Expense:
-    def __init__(self, payer, amount, participants):
-        self.payer = payer
-        self.amount = amount
-        self.participants = participants
